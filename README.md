ba773ba3cbb5a8e97625797f26ca9f512ba3331d
Stephen Benner <sbenner9@gmail.com>
2016-07-05 06:26:29 -0700

    added basic airOS interface script and other minor additions

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Water tank monitoring system
-This contents of this repository comprise the software necessary for management of a multi-component system which 
+This contents of this repository comprise the software necessary for management of a multi-component system purposed for the monitoring, recording, and reporting of water levels within an externally maintained water tank.
+
+## Components
+
+### *Controller* - Raspberry Pi
+
+### *Wireless Broadcast Station* - Ubiquiti NanoStation loco *M* airMAX
+
+### *Ultrasonic water level sensor*
+
+
+---
+### TODO:
+- secure remote access to ***loco M NanoStation*** 
---
+++ scripts/airOS.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+#
+# Utility for interfacing with 'Ubiquiti airOS' systems
+#
+# @author Stephen Benner
+# https://github.com/SteveBenner
+#
+require 'optparse'
+require 'yaml'
+
+# Default options
+opts = {}
+# Command line interface
+optparser = OptionParser.new do |cli|
+  cli.version = '0.0.1'
+  cli.summary_width  = 24
+  cli.summary_indent = ' ' * 2
+  cli.banner = "Utility for interfacing with 'Ubiquiti airOS' systems" + $/ +
+    'Usage: ${0} [options]'
+  cli.on_tail('-h', '--help', '--usage', 'Display this message.') { puts cli; exit }
+  cli.on_tail('--version', 'Display script version.') { puts cli.version; exit }
+end.parse!
+
+# @return [Hash] Key/value pairs contained within a .cfg file
+def parse_cfg_file(filename)
+  data = File.read filename
+  data.scan /(\S+)*=([^\n]+)/
+end
+
