d6b4243e36d102d0045febb1e33ccf4a7f2006ea
Stephen Benner <sbenner9@gmail.com>
2017-07-06 23:42:39 -0700

    modified script to use HTTP for couchDB calls

--- scripts/sensor_monitor.rb
+++ scripts/sensor_monitor.rb
@@ -3,19 +3,30 @@
 # This script is daemonized under CouchDB, and should run persistently.
 #
 require '../lib/helpers'
+require 'httparty'
 # require 'bundler'
 # Bundler.require :all
 
+# Setup database connection
+DB_SERVER = 'http://127.0.0.1:5984'
 DB_NAME = 'readings'
 
-$db_server = CouchRest.new 'http://127.0.0.1:5984'
-$db = $db_server.database DB_NAME
+# $db_server = CouchRest.new DB_SERVER
+# $db = $db_server.database DB_NAME
 
-# Distance in millimeters
-class DistanceReading < CouchRest::Model::Base
-  property :value, Float
-  timestamps!
+class CouchDB
+  include HTTParty
+  base_uri "#{DB_SERVER}/#{DB_NAME}"
+  format :json
+  headers 'Content-Type' => 'application/json' # note: MUST use old style hash syntax here
 end
+
+# Sensor reading (distance in millimeters)
+# class DistanceReading < CouchRest::Model::Base
+#   use_database DB_NAME
+#   property :value, Float
+#   timestamps!
+# end
 
 # todo: store these values in DB config
 SENSOR_POLL_INTERVAL = ENV['SENSOR_POLL_INTERVAL'] || 600 # seconds between sensor readings
@@ -28,7 +39,8 @@ loop do
   # todo: handle errors and bad output
 
   # Save the measurement to the database, converting it to a Float in the process
-  DistanceReading.create! value: reading.to_f
+  # DistanceReading.create! value: reading.to_f
+  CouchDB.post('', body: {value: reading.to_f, time: Time.now}.to_json)
 
   # todo: generate alert if water level val within specified threshold range
 
