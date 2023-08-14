448c112496e458a6f0e8afea4965e88b1c65388e
Sven <sven@neo-m2.local>
2023-08-14 12:08:28 -0700

    resolving 'v2-switch' branch

--- scripts/pi_receive.py
+++ scripts/pi_receive.py
@@ -13,7 +13,6 @@ RESET = DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 915.0)
 rfm69.encryption_key = ''
-
 packet = None
 
 while packet is None:
