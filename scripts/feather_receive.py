213d6a421876ac043445cd45447884c8ec7c984c
Robert Hancock <hancocklrobert@gmail.com>
2023-08-25 01:10:26 -0700

    Remote tank monitor bargraph removed; update new sensor distance measurement

--- scripts/feather_receive.py
+++ scripts/feather_receive.py
@@ -4,7 +4,6 @@
 import board, busio, digitalio, adafruit_rfm69
 from time import sleep
 from math import floor
-from adafruit_ht16k33.bargraph import Bicolor24
 import adafruit_ht16k33.segments
 
 # Radio Setup
@@ -16,66 +15,22 @@ rfm69.encryption_key = b'{\xc0\xee.5!d$!\xe2L@,$\r\xdb' # Yay, encryption!
 
 # Display Setup
 i2c = board.I2C()
-# 24 Segment Bargraph
-bg = Bicolor24(i2c, address=0x72)
 # Alphanumeric display
 dsply = adafruit_ht16k33.segments.Seg14x4(i2c, address=0x70, brightness=0.75)
 
-# To display water level on segment display
-def drawLevel(level):
-    if level == "wait":
-        bg.fill(bg.LED_YELLOW)
-        runs=0
-
-        while runs <= 2:
-            for i in range(22, 0, -1):
-                values = [i-1, i, i+1]
-
-                for x in values:
-                    bg[x] = bg.LED_OFF
-
-                sleep(0.003)
-                bg[i+2] = bg.LED_YELLOW
-
-            for i in range(1, 23, 1):
-                values = [i-1, i, i+1]
-
-                for x in values:
-                    bg[x] = bg.LED_OFF
-
-                sleep(0.003)
-                bg[i-2] = bg.LED_YELLOW
-
-            runs+=1
-
-        bg.fill(bg.LED_OFF)
-
-    else:
-        # 24 segments divided by max water level of 7.5ft = 3.2,
-        # slightly higher value to ensure full display gets used
-        ledSegments = floor(level*3.25)
-        if level <= 2.5:
-            color = bg.LED_RED
-        elif level < 4.99:
-            color = bg.LED_YELLOW
-        else:
-            color = bg.LED_GREEN
-        # Reset display
-        bg.fill(bg.LED_OFF)
-        # Loop for coloring, display placed upside down
-        for i in range(23, (23-ledSegments), -1):
-            bg[i] = color
 
 # To calculate fill level in feet
 def getLevel(level):
     # Distance between max fill level and actual fill level,
     # accounting for sensor placement of 1.6ft
-    correctedDistanceValue = level - 490
+    #correctedDistanceValue = level - 490
     # Max fill level is 7'5"
-    rawWaterLevel = 2286 - correctedDistanceValue
+    #rawWaterLevel = 2286 - correctedDistanceValue
     # Convert mm to feet
-    waterLevel = rawWaterLevel / 304.8
-    return(waterLevel)
+    #waterLevel = rawWaterLevel / 304.8
+    waterLevel = 2743.2 - level
+    cnvtdWaterLevel = (waterLevel * 0.00328084)
+    return(cnvtdWaterLevel)
 
 while True:
     # Try and receive value
@@ -101,10 +56,8 @@ while True:
         dsply.show()
         dsply.print(str(round(waterLevel, 3))[:-1] + 'F')
         dsply.show()
-        drawLevel(waterLevel)
 
     else:
         print("Waiting for signal..")
-        drawLevel("wait")
 
     sleep(10)
