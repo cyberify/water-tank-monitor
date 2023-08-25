8c20f54c79da08ce9923ab57f39e1cfc1aee3cc4
Stephen Benner <developer@sensiblesolutions.me>
2023-08-24 23:19:01 -0700

    DONG

--- scripts/monitor.rb
+++ scripts/monitor.rb
@@ -21,6 +21,7 @@ SENSOR_SCRIPT = File.expand_path 'pi_receive.py', __dir__
 CONFIG = CouchDB.get('/admin/config').to_hash.transform_keys &:to_sym
 
 SENSOR_POLL_INTERVAL = CONFIG[:poll_interval_sensor] # Seconds between sensor readings
+EMPTY = 2286 # Maximum distance in mm from the sensor to the tank bottom
 
 #
 # MAIN LOOP
@@ -34,12 +35,28 @@ loop do
   # Save the measurement to the database
   CouchDB.post '/readings', body: { value: sensor_reading.to_i, "_id": Time.now.utc.iso8601 }.to_json
 
-  # todo: generate alert if water level val within specified threshold range
-  # todo: generate alert if reading is 0 (indicating overflow)
+  # Alerts
+  if sensor_reading >= CONFIG[:threshold_low]
+    tank_level = EMPTY - sensor_reading
+    msg = "WARNING! The tank level is below #{tank_level}mm!"
+    # call telegram alert bot
+    # Log the alert
+    CouchDB.post '/logs', body:
+      { type: 'alert', category: 'level_low', level: tank_level, "id": Time.now.utc.iso8601 }.to_json
+  end
+  if sensor_reading == CONFIG[:threshold_high]
+    tank_level = EMPTY - sensor_reading
+    msg = 'WARNING! The tank is overflowing!'
+    # call telegram alert bot
+    # Log the alert
+    CouchDB.post '/logs', body:
+      { type: 'alert', category: 'level_high', level: tank_level, "id": Time.now.utc.iso8601 }.to_json
+  end
 
   # Catch *ANY* error occurring while running the master script, logging it and generating an alert
-  rescue Exception
-    # todo: ERROR LOGGING CODE GOES HERE
+  rescue Exception => e
+    CouchDB.post '/logs', body:
+      { type: 'alert', category: 'script_error', error: e, "id": Time.now.utc.iso8601 }.to_json
   end
 
   sleep SENSOR_POLL_INTERVAL.to_f
