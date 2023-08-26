86435cc0b00fbb874ce27fb92e2b0c9908840fb1
Robert Hancock <hancocklrobert@gmail.com>
2023-08-26 11:13:17 -0700

    Add quotes for proper variable expansion

--- scripts/notify_group.sh
+++ scripts/notify_group.sh
@@ -3,4 +3,4 @@
 chat_id=""
 bot_token=""
 
-curl -s -X POST https://api.telegram.org/$bot_token/sendMessage -d chat_id=$chat_id -d text=$1
+curl -s -X POST https://api.telegram.org/$bot_token/sendMessage -d chat_id=$chat_id -d text="$1"
